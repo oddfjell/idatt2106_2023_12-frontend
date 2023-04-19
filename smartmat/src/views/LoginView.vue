810c2c8553cab2d124571d25f8690cf717172fea
ingrid <ingrfla@stud.ntnu.no>
2023-04-19 15:02:33 +0200

    Implemented start of login field with some css style

--- smartmat/src/views/LoginView.vue
+++ smartmat/src/views/LoginView.vue
@@ -1,6 +1,16 @@
 <template>
-  <div >hei </div>
+  <form class="loginField">
 
+<div class="inputField">
+    <label for="username">Username </label>
+    <input type="text" placeholder="Skriv ditt brukernavn " name="username" required>
+
+    <label for="password">Password </label>
+    <input type="text" placeholder="Skriv ditt passord" name="password" required>
+
+    <button type="submit">Submit</button>
+    </div>
+  </form>
 
 
 </template>
@@ -9,5 +19,18 @@
 </script>
 
 <style>
+
+.loginField{
+  max-width: 50%;
+  margin:0;
+  display: grid;
+  grid-row-gap: 1rem;
+  align-items: center;
+}
+
+
+
+
+
 
 </style>
