a9b828fdbc5ea03a5aabc3c15bdc5fb6583ceb2c
Abhirami <abhiramk@stud.ntnu.no>
2023-04-20 14:52:49 +0200

    added image for each choice and an user icon

--- smartmat/src/views/HomeView.vue
+++ smartmat/src/views/HomeView.vue
@@ -1,22 +1,34 @@
 <template>
 
+  <fa icon="user" class="fa-icon" />
+
+
   <div id="border">
     <h1>Kjøleskap</h1>
+    <img src="../images/refrigerator.png" alt="Kjøleskap">
+
+
+
 
   </div>
 
   <div id="border">
+
     <h1>Ukemeny</h1>
+    <img id="menu"
+        src="../images/weeklyMenuPage.png" alt="Ukemeny">
+
 
 
   </div>
 
   <div id="border">
     <h1>Handleliste</h1>
+    <img src="../images/list.png" alt="Handleliste">
+
 
 
   </div>
-
 
 
 
