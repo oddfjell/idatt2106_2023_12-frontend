7ef5c756befb2357cd85457d85386fd3135d144f
Abhirami <abhiramk@stud.ntnu.no>
2023-04-21 13:09:21 +0200

    corrected import frontPage.css in main.css

--- smartmat/src/components/Navbar.vue
+++ smartmat/src/components/Navbar.vue
@@ -1,5 +1,16 @@
 <template>
+  <header>
 
+    <HelloWorld msg="Hello!" />
+
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/login">| Login</RouterLink>
+      <RouterLink to="/register"> | Register</RouterLink>
+
+    </nav>
+
+  </header>
 </template>
 
 <script>
