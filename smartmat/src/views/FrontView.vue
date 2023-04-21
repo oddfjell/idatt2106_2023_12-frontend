60e96194165d603ae8892fe0bf12feef88bdb8ce
Abhirami <abhiramk@stud.ntnu.no>
2023-04-21 10:52:57 +0200

    implemented router to redirect to correct page when pushed

--- smartmat/src/views/FrontView.vue
+++ smartmat/src/views/FrontView.vue
@@ -1,41 +1,42 @@
 <template>
 
+
+
   <fa icon="user" class="fa-icon" />
 
   <div id="border">
-    <h1>Kjøleskap</h1>
-    <img src="../images/refrigerator.png" alt="Kjøleskap">
+    <nav>
+      <RouterLink to="/Refrigerator">  <h1>Kjøleskap</h1>     <img src="../images/refrigerator.png" alt="Kjøleskap">
+    </RouterLink>
 
-
-
-
-  </div>
-
-  <div id="border">
-
-    <h1>Ukemeny</h1>
-    <img id="menu"
-         src="../images/weeklyMenuPage.png" alt="Ukemeny">
+    </nav>
 
 
 
   </div>
 
   <div id="border">
-    <h1>Handleliste</h1>
-    <img src="../images/list.png" alt="Handleliste">
+
+    <nav>
+
+    <RouterLink to="/WeeklyMenuView">  <h1>Ukemeny</h1>  <img id="menu" src="../images/weeklyMenuPage.png" alt="Ukemeny">
+    </RouterLink>
+    </nav>
+
+
+  </div>
+
+  <div id="border">
+    <nav>
+      <RouterLink to="/ShoppingList">  <h1>Handleliste</h1> <img src="../images/list.png" alt="Handleliste">
+      </RouterLink>
+
+    </nav>
 
 
 
   </div>
 
-
-
-
-<nav>
-  <RouterLink to="/frontView">| FrontView </RouterLink>
-
-</nav>
 
 
 
