1749e6aba362e93b7bec17f0f74d5262ea9a189b
Abhirami <abhiramk@stud.ntnu.no>
2023-04-21 14:29:32 +0200

    removed unnecessary router

--- smartmat/src/router/index.js
+++ smartmat/src/router/index.js
@@ -26,14 +26,6 @@ const router = createRouter({
       component: () => import('../views/RegisterUserView.vue')
     },
     {
-      path: '/frontView',
-      name: 'frontView',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/FrontView.vue')
-    },
-    {
       path: '/WeeklyMenuView',
       name: 'WeeklyMenuView',
       component: () => import('../views/WeeklyMenuView.vue')
