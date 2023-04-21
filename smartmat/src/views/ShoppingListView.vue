024907a93785d54f4b353910025edb483c7936ab
EmilOrv <emiloo@stud.ntnu.no>
2023-04-21 14:42:39 +0200

    Added ShoppingList container and api-call to groceries

--- smartmat/src/views/ShoppingListView.vue
+++ smartmat/src/views/ShoppingListView.vue
@@ -1,11 +1,28 @@
 <template>
-
+  <div class="container">
+  <h1>Handleliste</h1>
+  </div>
 </template>
 
 <script>
+
+import shoppingListService from "@/services/shoppingListService";
+
 export default {
-  name: "ShoppingList"
+  name: "ShoppingList",
+    groceries:[],
+
+
+    async created() {
+       let groceries = await shoppingListService.getProducts()
+        for(let grocery of groceries){
+            this.groceries.push(grocery)
+        }
+        console.log(this.groceries)
+    }
 }
+
+
 </script>
 
 <style scoped>
