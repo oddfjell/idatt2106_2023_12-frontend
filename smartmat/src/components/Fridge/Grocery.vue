6041152fc925c054b44eaa442404f1a5f536a214
EmilOrv <emilols.eo@gmail.com>
2023-04-23 15:45:01 +0200

    Initialized the GroceryComponent with Throw Button

---
+++ smartmat/src/components/Fridge/Grocery.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <h1>{{grocery.name}}</h1>
+        <p>{{grocery.category.name}}</p>
+        <p>Antall: {{count}}</p>
+        <div class="buttonBar">
+            <button >Eat</button>
+            <Throw/>
+        </div>
+    </div>
+    <div class=""></div>
+</template>
+
+<script>
+import Throw from "@/components/Fridge/Throw.vue";
+
+export default {
+    name: "GroceryComponent",
+    components: {Throw},
+    props: {
+        grocery:Object,
+        count:Number,
+    },
+    created() {
+        console.log(this.grocery)
+    },
+}
+</script>
+
+<style scoped>
+.buttonBar{
+    float:right;
+}
+</style>
---
+++ smartmat/src/components/Fridge/Throw.vue
@@ -0,0 +1,82 @@
+<template>
+    <button @click="showThrowModal=true"> Kast </button>
+    <Teleport to="body">
+        <BaseModal :show="showThrowModal" @close="showThrowModal = false">
+            <template #header>
+                <h3>Kast</h3>
+            </template>
+            <template #body>
+                <p>Hvor mye var det igjen av varen?</p>
+                <div class="slidecontainer">
+                <input name="slider" tabindex="0" type="range" step="10" min="0" max="100" value="100" class="slider" @input="onSliderInput">
+                </div>
+                <p v-if="value>90">Hele varen ble kastet :(</p>
+                <p v-else-if="value<10">Ingenting! Hele varen var brukt! :)</p>
+                <p v-else>{{value}}% igjen</p>
+            </template>
+            <template #footer>
+            <button class="Btn" @click="$emit('close')" id="throwBtn">Kast</button>
+                <button class="Btn" @click="$emit('close')" id="cancelBtn" >Avbryt</button>
+            </template>
+        </BaseModal>
+    </Teleport>
+</template>
+
+<script>
+import BaseModal from "@/components/Common/BaseModal.vue";
+
+export default {
+    name: "ThrowComponent",
+    components: {BaseModal},
+    data() {
+        return {
+            showThrowModal: false,
+            value:100
+        }
+    },
+    methods:{
+        onSliderInput(event){
+            this.value=event.target.value;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.slidecontainer {
+    width: 100%;
+}
+.slider{
+    width: 100%;
+}
+.Btn{
+    float: right;
+    border: 0;
+    padding: 10px;
+    width: 25%;
+    border-radius: 5px;
+    margin: 0;
+}
+
+#throwBtn{
+    background: #0b6dff;
+    color: white;
+}
+
+#cancelBtn{
+    background: #b7b7b7;
+    color: black;
+    margin-right: 10px;
+}
+
+#throwBtn:hover {
+    background-color: #4169a8;
+    cursor: pointer;
+}
+
+#cancelBtn:hover {
+    background-color: #d0cece;
+    cursor: pointer;
+}
+
+</style>
