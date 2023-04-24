482eeab8ea80e1d648cda35de50dbcda358b3acf
Abhirami <abhiramk@stud.ntnu.no>
2023-04-24 13:00:04 +0200

    added icons on the navbar and router links for them

--- smartmat/src/components/Navbar.vue
+++ smartmat/src/components/Navbar.vue
@@ -6,14 +6,34 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
 
-    <fa icon="user" class="fa-icon-user" />
-    <fa icon="cart-shopping" class="fa-icon-cart"/>
+    <RouterLink to="/ShoppingListView" tag ="button">
+    <fa :icon="['fas', 'cart-shopping']" class="fa-icon-cart"/>
+      </RouterLink>
+
+    <RouterLink to="/" tag ="button">
+    <fa :icon="['fas', 'home']" class="fa-icon-home" />
+    </RouterLink>
+
+
+    <fa :icon="['fas', 'user']" class="fa-icon-user" />
+
+
+    <RouterLink to="/WeeklyMenuView" tag ="button" >
+    <fa :icon="['fas', 'cutlery']" class="fa-icon-cutlery" />
+    </RouterLink>
+
+    <RouterLink to="/RefrigeratorView" tag ="button">
+      <img class="fridge" src="../images/fridge.svg" alt="Kjøleskap">
+    </RouterLink>
 
     <nav>
       <RouterLink to="/">Hjem</RouterLink>
       <RouterLink to="/weeklyMenuView"> | Ukemeny</RouterLink>
       <RouterLink to="/shoppingListView"> | Handleliste</RouterLink>
       <RouterLink to="/refrigeratorView"> | Kjøleskap</RouterLink>
+
+
+
 
 
 
