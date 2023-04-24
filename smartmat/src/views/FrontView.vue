6caa0c6e99c7fd421e12430673a74cf29bcb1c55
Abhirami <abhiramk@stud.ntnu.no>
2023-04-24 12:59:07 +0200

    made whole box reactive to click, not only image and writing

--- smartmat/src/views/FrontView.vue
+++ smartmat/src/views/FrontView.vue
@@ -3,38 +3,34 @@
 
 
 
-  <div id="border">
-    <nav>
-      <RouterLink to="/Refrigerator">  <h1>Kjøleskap</h1>     <img src="../images/refrigerator.png" alt="Kjøleskap">
+      <RouterLink to="/Refrigerator">
+        <div id="border">
+        <h1>Kjøleskap</h1>   <img src="../images/refrigerator.png" alt="Kjøleskap" >
+        </div>
     </RouterLink>
 
-    </nav>
 
 
 
-  </div>
-
-  <div id="border">
-
-    <nav>
-
-    <RouterLink to="/WeeklyMenuView">  <h1>Ukemeny</h1>  <img id="menu" src="../images/weeklyMenuPage.png" alt="Ukemeny">
+    <RouterLink to="/WeeklyMenuView">
+      <div id="border">
+        <h1>Ukemeny</h1>
+        <img id="menu" src="../images/weeklyMenuPage.png" alt="Ukemeny">
+      </div>
     </RouterLink>
-    </nav>
-
-
-  </div>
-
-  <div id="border">
-    <nav>
-      <RouterLink to="/ShoppingList">  <h1>Handleliste</h1> <img src="../images/list.png" alt="Handleliste">
-      </RouterLink>
-
-    </nav>
 
 
 
-  </div>
+
+    <RouterLink to="/ShoppingList">
+      <div id="border">
+        <h1>Handleliste</h1>
+        <img src="../images/list.png" alt="Handleliste">
+      </div>
+    </RouterLink>
+
+
+
 
 
 
