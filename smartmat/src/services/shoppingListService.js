1d8f22a1372da1acc68ee0b1580d107e94855428
EmilOrv <emiloo@stud.ntnu.no>
2023-04-25 10:43:16 +0200

    Added Api-calls for update checked and buy checked

--- smartmat/src/services/shoppingListService.js
+++ smartmat/src/services/shoppingListService.js
@@ -17,11 +17,18 @@ export default {
         return axios.get(baseURL+'/', config(token))//Var add header
     },
     addToShoppingList(product, token){
-        return axios.post(baseURL+'/', product, config(token))
+        return axios.post(baseURL+'/add', product, config(token))
     },
     removeFromShoppingList(productID, token){
         return axios.delete(baseURL+'/remove/'+productID, config(token))
     },
+    updateChecked(checked, token){
+        return axios.post(baseURL+'/updateChecked', checked, config(token))
+    },
+    buyChecked(token){
+        return axios.post(baseURL+'/buyCheked', config(token))
+    },
+
     acceptRequest(product, token){
         return axios.put(baseURL+'/accept', product, config(token))
     }
