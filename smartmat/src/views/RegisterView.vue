718d7fe71ca49866589c0f33d417699ce10934b5
SchatzerZD <daniel.walaker@gmail.com>
2023-04-26 13:17:04 +0200

    Fixed same bug in register

--- smartmat/src/views/RegisterView.vue
+++ smartmat/src/views/RegisterView.vue
@@ -69,7 +69,7 @@ export default {
         }
     },
     created() {
-        if(tokenStore().user.username !== null ){
+        if(tokenStore().user.username !== ""){
             router.push("/home")
         }
     }
