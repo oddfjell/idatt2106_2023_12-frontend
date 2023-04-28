d556a32689dac4b5aa9115ab51ae8430c2a03241
EmilOrv <emiloo@stud.ntnu.no>
2023-04-28 19:52:38 +0200

    Added info-text and addEntity saves to store

--- smartmat/src/views/ShoppingListView.vue
+++ smartmat/src/views/ShoppingListView.vue
@@ -7,14 +7,17 @@
                 :placeholder="selectedText"
               v-on:selected="onSelection">
       </Dropdown>
-          <button @click="addShoppingListEntity">Legg til vare</button>
+          <button class="Btn GreyBtn" @click="addShoppingListEntity">Legg til vare</button>
       </div>
       <div class="container" id="shoppingGridContainer">
+          <p id="info">{{info}}</p>
           <div id="buttonBar" class="Btn">
               <button class="BlueBtn" id="saveChanges" @click="save">Lagre endringer</button>
               <button class="BlueBtn" id="addSelected" @click="buy">Kjøp valgte varer</button>
           </div>
-     <ShoppingListGrid/>
+          <div v-if="loading"><h3 style="text-align: center">Laster ...</h3></div>
+     <ShoppingListGrid ref="grid" v-else-if="shoppingListStore().getShoppingListEntities().length" :update="trigger"/>
+          <div v-else><h3 style="text-align: center"> Du har ikke noe i handlelisten</h3></div>
       </div>
   </div>
   <div v-else>
@@ -32,57 +35,79 @@ import ShoppingListGrid from "@/components/ShoppingList/shoppingListGrid.vue";
 import router from "@/router";
 import {shoppingListStore} from "@/stores/shoppingListStore";
 
-
 export default {
   name: "ShoppingList",
     components:{ShoppingListGrid, Dropdown},
     data(){
       return{
           selectedText:"Søk etter vare",
-          amount:1,
           selected:null,
           groceries:[],
-          changes:[]
+          loading:true,
+          trigger:false,
+          info:" ",
+          saved:true
       }
     },
     methods:{
+        shoppingListStore,
         onSelection(selection) {
-            this.selected = selection;
-            this.selectedText=selection.name
-            console.log(selection.name + " has been selected");
+            if(selection!==null && selection !==undefined) {
+                this.selected = selection;
+                this.selectedText = selection.name
+            } else{
+                this.selectedText="Søk etter vare"
+            }
         },
         async buy(){
             try {
                 await this.save()
-                console.log(tokenStore().user.jwt)
+                this.saved=true
+                this.info = "Legger til i handleliste ..."
                 await shoppingListService.buyChecked(tokenStore().user.jwt)
-                location.reload()
+                shoppingListStore().setShoppingListEntities([])
+                this.info = "Lagt til!"
             }catch (error){
                 console.log(error)
             }
         },
         async save(){
+            this.info="Lagrer... "
             try{
-                await shoppingListService.saveChanges(shoppingListStore().getChanges(), tokenStore().user.jwt)
-                await shoppingListStore().removeChanges()
+                await shoppingListService.saveChanges(shoppingListStore().getShoppingListEntities(), tokenStore().user.jwt)
+                this.saved=true
+                this.info="Lagret!"
             } catch (error){
                 console.log(error)
+                this.info="Kunne ikke lagre"
             }
         },
         async addShoppingListEntity(){
-            let product = {name:this.selected.name, count: this.amount}
-            try {
-                await shoppingListService.addToShoppingList(product, tokenStore().user.jwt)
-                location.reload()
-            }catch (error){
-              console.log(error)
-            }finally {
-                this.selected=null
-                this.amount = 1
-            }
+            let product = {name:this.selected.name, count: 1, foundInStore:false}
+            shoppingListStore().addShoppingListEntity(product)
+            this.trigger=!this.trigger
+            this.onSelection(null)
+            if(this.$refs.grid){this.$refs.grid.updateChecked()}
         },
     },
     async created() {
+      //get shoppingListEntities and update Store
+        let listEntities = []
+        try {
+            let shoppingListResponse = await shoppingListService.getProducts(tokenStore().user.jwt)
+            let shoppinglistEntities = shoppingListResponse.data
+            for (let shoppinglistEntity of shoppinglistEntities) {
+                listEntities.push(shoppinglistEntity)
+            }
+        }catch (error){
+            console.log(error)
+        }finally
+        {
+            this.loading=false
+        }
+        shoppingListStore().setShoppingListEntities(listEntities)
+
+        //Get groceries from database
        let groceriesResponse = await groceryService.getProducts(tokenStore().user.jwt)
         let groceries = groceriesResponse.data
         for(let grocery of groceries){
@@ -92,6 +117,14 @@ export default {
     mounted() {
         if(!tokenStore().user.username){
             router.push("/")
+        }
+    },
+    async beforeUnmount() {
+        if(!this.saved){
+            if(confirm("You have unsaved changes. Save?")){
+                await this.save()
+                alert("Saved!")
+            }
         }
     },
     computed:{
@@ -106,8 +139,11 @@ export default {
 
 <style scoped>
 .container{
+    max-width: 100vh;
     min-height: 50vh;
     border: 1px solid steelblue;
+    padding-bottom: 0;
+    padding-top: 0;
 }
 #header{
     display: flex;
@@ -127,5 +163,16 @@ export default {
 #buttonBar{
     display: flex;
     justify-content: space-evenly;
+    margin: 10px;
+}
+#info{
+    text-align: center;
+    font-size: large;
+    margin: 0;
+    padding:0;
+}
+.GreyBtn{
+    padding-top: 2px;
+    padding-bottom: 2px;
 }
 </style>
