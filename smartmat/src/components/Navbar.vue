c4eb0174fb2220b4780ee92e3f0fc2875a33a66e
Abhirami <abhiramk@stud.ntnu.no>
2023-04-28 14:43:10 +0200

    added customized logo

--- smartmat/src/components/Navbar.vue
+++ smartmat/src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <header>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <div id="logo-frame">
+    <img id="logo" src="../images/logo.png">
+    </div>
+    <div id="placeholder-space">
+    </div>
     <div  id="navbar-frame" >
     <nav>
       <div id="nav-icons" v-if="username" >
