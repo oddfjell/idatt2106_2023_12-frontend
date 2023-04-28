19b94dd1ec6d4279823ec60a93a1e6b8ca47f11d
Abhirami <abhiramk@stud.ntnu.no>
2023-04-28 12:15:48 +0200

    removed icons except home icon when on homepage

--- smartmat/src/components/Navbar.vue
+++ smartmat/src/components/Navbar.vue
@@ -1,24 +1,41 @@
 <template>
   <header>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <div id="navbar-frame">
+    <div  id="navbar-frame" >
     <nav>
-      <div v-if="username" id="nav-icons">
-        <RouterLink to="/home">
-            <span class="material-symbols-outlined">home</span>
-        </RouterLink>
-        <RouterLink to="/refrigerator">
-          <span class="material-symbols-outlined">kitchen</span>
-        </RouterLink>
-        <RouterLink to="/weeklyMenu">
-          <span class="material-symbols-outlined">restaurant</span>
-        </RouterLink>
-    <RouterLink to="/shoppingList">
-      <span class="material-symbols-outlined">shopping_cart</span>
-      </RouterLink>
+      <div id="nav-icons" v-if="username" >
 
+        <div id="all-icons-bottom" v-if="$route.path=='/home'" >
+          <RouterLink to="/home">
+            <span class="material-symbols-outlined">home</span>
+          </RouterLink>
+          <RouterLink to="/refrigerator">
+            <span class="material-symbols-outlined">kitchen</span>
+          </RouterLink>
+          <RouterLink to="/weeklyMenu">
+            <span class="material-symbols-outlined">restaurant</span>
+          </RouterLink>
+          <RouterLink to="/shoppingList">
+            <span class="material-symbols-outlined">shopping_cart</span>
+          </RouterLink>
+        </div>
+
+        <div id="except-user-top" v-if="$route.path !=='/home'">
+          <RouterLink to="/home">
+              <span class="material-symbols-outlined">home</span>
+          </RouterLink>
+          <RouterLink to="/refrigerator">
+            <span class="material-symbols-outlined">kitchen</span>
+          </RouterLink>
+          <RouterLink to="/weeklyMenu">
+            <span class="material-symbols-outlined">restaurant</span>
+          </RouterLink>
+          <RouterLink to="/shoppingList">
+            <span class="material-symbols-outlined">shopping_cart</span>
+          </RouterLink>
+        </div>
         <RouterLink to="/profile" >
-<span class="material-symbols-outlined">person</span>
+          <span class="material-symbols-outlined">person</span>
         </RouterLink>
       </div>
 
