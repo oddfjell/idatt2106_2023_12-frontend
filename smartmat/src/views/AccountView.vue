8998ee6060b3b32aec5748b87993bd44658eb8a1
EmilOrv <emilols.eo@gmail.com>
2023-05-03 21:57:49 +0200

    Made frontend ready for a "remove account" functionality

--- smartmat/src/views/AccountView.vue
+++ smartmat/src/views/AccountView.vue
@@ -11,6 +11,8 @@
       <button @click="changeProfile" id="changeProfile" class="logout-Btn">Bytt bruker</button>
       <button @click="logout" id="logout" class="logout-Btn">Logg ut
         <span class="material-symbols-outlined">logout</span></button>
+
+        <button @click="removeAccount" id="removeAccount" class="logout-Btn">Slett konto</button>
     </div>
 
 
@@ -24,6 +26,7 @@
 <script>
 import {tokenStore} from "@/stores/tokenStore";
 import router from "@/router";
+import accountService from "@/services/accountService";
 
 export default {
   name: "ProfileView",
@@ -50,7 +53,20 @@ export default {
     changeProfile() {
       tokenStore().changeUsername("")
       router.push("/profile")
-    }
+    },
+      removeAccount(){
+        if(confirm("Er du sikker på at du vil slette kontoen? All data vil bli tapt!")){
+            try{
+                accountService.removeAccount(tokenStore().user.jwt)
+                tokenStore().changeJWT("")
+                tokenStore().changeUsername("")
+                router.push("/")
+                alert("Din konto ble slettet")
+            }catch (error){
+                console.log(error)
+            }
+        }
+      }
   },
   created() {
     console.log(tokenStore().user.username)
