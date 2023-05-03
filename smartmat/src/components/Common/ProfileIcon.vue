859186983282e80d9a8a746b13e40977a9e68397
EmilOrv <emilols.eo@gmail.com>
2023-05-03 22:31:33 +0200

    Removed "-" from add

--- smartmat/src/components/Common/ProfileIcon.vue
+++ smartmat/src/components/Common/ProfileIcon.vue
@@ -1,7 +1,7 @@
 <template>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
   <div id="profile-card">
-      <div class="material-symbols-outlined" id="remove" @click="removeProfile">Remove</div>
+      <div v-if="!add" class="material-symbols-outlined" id="remove" @click="removeProfile">Remove</div>
       <div id="choose" @click="selectProfile">
         <div v-if="!add" class="material-symbols-outlined" id="icon">person</div>
         <div v-else class="material-symbols-outlined" id="icon">add</div>
