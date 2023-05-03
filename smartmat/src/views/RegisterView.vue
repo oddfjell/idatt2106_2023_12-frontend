389147a4b5e3bee05d4d0574d27cc2e7c0111544
ingrid <ingrfla@stud.ntnu.no>
2023-05-03 13:50:01 +0200

    refactor css files

--- smartmat/src/views/RegisterView.vue
+++ smartmat/src/views/RegisterView.vue
@@ -1,31 +1,33 @@
 <template>
-    <div class="container">
-        <h1>Registrer</h1>
-        <form @submit.prevent >
-            <!-- Label and input for the username field -->
-            <label class="registerLabel" for="username">Brukernavn: </label>
-            <input type="text" v-model="user.username" placeholder="Skriv ditt brukernavn " name="username" required id="register-username">
+  <div class="container">
+    <h1>Registrer</h1>
+    <form @submit.prevent>
+      <!-- Label and input for the username field -->
+      <label class="registerLabel" for="username">Brukernavn: </label>
+      <input type="text" v-model="user.username" placeholder="Skriv ditt brukernavn " name="username" required
+             id="register-username">
 
-            <!-- Label and input for the password field -->
-            <label class="registerLabel" for="password">Passord: </label>
-            <input type="password" v-model="user.password" placeholder="Skriv ditt passord" name="password" required id="register-password">
+      <!-- Label and input for the password field -->
+      <label class="registerLabel" for="password">Passord: </label>
+      <input type="password" v-model="user.password" placeholder="Skriv ditt passord" name="password" required
+             id="register-password">
 
-            <!-- Text field for displaying error messages -->
-            <p id="error" v-if="error">{{error}} </p>
+      <!-- Text field for displaying error messages -->
+      <p id="error" v-if="error">{{ error }} </p>
 
-            <!-- Button for registering user  -->
-            <div class="Btn">
-                <button class="BlueBtn" id="registerBtn" @click="onSubmit" type="submit">Registrer</button>
-            </div>
+      <!-- Button for registering user  -->
+      <div class="Btn">
+        <button class="BlueBtn" id="registerBtn" @click="onSubmit" type="submit">Registrer</button>
+      </div>
 
-            <!-- Button for redirecting to login-page -->
-            <div class="Btn">
-                <button class="GreyBtn" id="loginBtn" @click="onLogin" >Har du allerede konto? Logg inn her!</button>
-            </div>
+      <!-- Button for redirecting to login-page -->
+      <div class="Btn">
+        <button class="GreyBtn" id="loginBtn" @click="onLogin">Har du allerede konto? Logg inn her!</button>
+      </div>
 
-        </form>
+    </form>
 
-    </div>
+  </div>
 </template>
 <script>
 import accountService from "@/services/accountService";
@@ -33,75 +35,71 @@ import router from "@/router";
 import {tokenStore} from "@/stores/tokenStore";
 
 export default {
-    /**
-     * Register-credentials and errormessage
-     * @returns {{error: string, user: {password: string, username: string}}}
-     */
-    data() {
-        return {
-            user: {
-                password: "",
-                username: ""
-            },
-            error:""
-        }
-    },
-    methods:{
-        /**
-         * Triggerd when clicking the register-button. Sends a register-request. If it is valid it sends a login-request which sets the token and username as the
-         * logged-in user. Redirects to the homepage if successful
-         * @returns {Promise<void>}
-         */
-        async onSubmit(){
-            console.log(this.user.username)
-            if(this.user.username !== undefined || "" ){
-                try {
-                    let response = await accountService.registerAccount(this.user)
-                    if (response.status === 200) {
-                        try {
-                            response = await accountService.loginAccount(this.user)
-                            if(response.status===200){
-                                let data= response.data
-                                tokenStore().changeJWT(data.jwt)
-                                tokenStore().changeUsername(data.username)
-                                await router.push("/profile")
-                            }
-                        }catch (error){
-                            await router.push("/")
-                        }
-                    } else {
-                        console.log("Login failed")
-                        this.error=response.status
-                    }
-                }catch (error){
-                    console.log("Registration failed")
-                    this.error="Registration failed"
-                }
-            }
-        },
-        /**
-         * Redirects to the login-page
-         */
-        onLogin(){
-            router.push('/')
-        }
-    },
-    /**
-     * If user is already logged in, the frontpage will be displayed instead.
-     */
-    created() {
-        if(tokenStore().user.username !== ""){
-            router.push("/profile")
-        }
+  /**
+   * Register-credentials and errormessage
+   * @returns {{error: string, user: {password: string, username: string}}}
+   */
+  data() {
+    return {
+      user: {
+        password: "",
+        username: ""
+      },
+      error: ""
     }
+  },
+  methods: {
+    /**
+     * Triggerd when clicking the register-button. Sends a register-request. If it is valid it sends a login-request which sets the token and username as the
+     * logged-in user. Redirects to the homepage if successful
+     * @returns {Promise<void>}
+     */
+    async onSubmit() {
+      console.log(this.user.username)
+      if (this.user.username !== undefined || "") {
+        try {
+          let response = await accountService.registerAccount(this.user)
+          if (response.status === 200) {
+            try {
+              response = await accountService.loginAccount(this.user)
+              if (response.status === 200) {
+                let data = response.data
+                tokenStore().changeJWT(data.jwt)
+                tokenStore().changeUsername(data.username)
+                await router.push("/profile")
+              }
+            } catch (error) {
+              await router.push("/")
+            }
+          } else {
+            console.log("Login failed")
+            this.error = response.status
+          }
+        } catch (error) {
+          console.log("Registration failed")
+          this.error = "Registration failed"
+        }
+      }
+    },
+    /**
+     * Redirects to the login-page
+     */
+    onLogin() {
+      router.push('/')
+    }
+  },
+  /**
+   * If user is already logged in, the frontpage will be displayed instead.
+   */
+  created() {
+    if (tokenStore().user.username !== "") {
+      router.push("/profile")
+    }
+  }
 }
 </script>
 
 <style scoped>
-
 @import "../assets/style/register.css";
-
-
-
 </style>
 
--- smartmat/src/views/WeeklyMenuView.vue
+++ smartmat/src/views/WeeklyMenuView.vue
@@ -1,40 +1,42 @@
 <template>
-    <div v-if="username">
-  <Carousel ref="carousel" :wrap-around="false" :items-to-show="1" id="carousel">
-    <Slide v-for="recipe in displayRecipes" :key="recipe">
-      <div class="carousel__item" :style="{backgroundImage: 'url(' + recipe.image + ')' }">
+  <div v-if="username">
+    <Carousel ref="carousel" :wrap-around="false" :items-to-show="1" id="carousel">
+      <Slide v-for="recipe in displayRecipes" :key="recipe">
+        <div class="carousel__item" :style="{backgroundImage: 'url(' + recipe.image + ')' }">
           <h3 id="slideTitle"> {{ recipe.title }} </h3></div>
-    </Slide>
-    <template #addons>
-      <Navigation />
-      <button id="refreshRecipeBtn" @click="getAndChangeRecipe">
-        <img src="../images/refresh-icon.png" alt="refresh-icon">
+      </Slide>
+      <template #addons>
+        <Navigation/>
+        <button id="refreshRecipeBtn" @click="getAndChangeRecipe">
+          <img src="../images/refresh-icon.png" alt="refresh-icon">
+        </button>
+      </template>
+    </Carousel>
+    <p id="info">{{ info }}</p>
+    <div class=" Btn addToShoppingList">
+      <button class="BlueBtn" @click="addToShoppingList">
+        Legg til varer i handleliste
       </button>
-    </template>
-  </Carousel>
-        <p id="info">{{info}}</p>
-        <div class=" Btn addToShoppingList">
-            <button class="BlueBtn" @click="addToShoppingList">
-                Legg til varer i handleliste</button>
-        </div>
-        <div class="container" v-if="this.carousel">
-            <h1 class="ingredients-title">Ingredienser</h1>
-        <p style="display: none">{{currentSlide}}</p>
-            <div v-if="this.displayRecipes[currentSlide.value]">
-            <a :href="this.displayRecipes[currentSlide.value].url">Se på matprat</a>
-            <p class="ingredients-text" :key="this.displayRecipes[currentSlide._value] + index" v-for="(ingredient, index) in this.displayRecipes[currentSlide._value].ingredients ">{{ingredient}}</p>
-            </div>
-        </div>
-
     </div>
+    <div class="container" v-if="this.carousel">
+      <h1 class="ingredients-title">Ingredienser</h1>
+      <p style="display: none">{{ currentSlide }}</p>
+      <div v-if="this.displayRecipes[currentSlide.value]">
+        <a :href="this.displayRecipes[currentSlide.value].url">Se på matprat</a>
+        <p class="ingredients-text" :key="this.displayRecipes[currentSlide._value] + index"
+           v-for="(ingredient, index) in this.displayRecipes[currentSlide._value].ingredients ">{{ ingredient }}</p>
+      </div>
+    </div>
+
+  </div>
   <div v-else><h1>Please log in</h1></div>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import { Carousel, Navigation, Slide } from 'vue3-carousel'
+import {defineComponent, ref} from 'vue'
+import {Carousel, Navigation, Slide} from 'vue3-carousel'
 import recipeService from '../services/recipeService'
-import { tokenStore } from "@/stores/tokenStore";
+import {tokenStore} from "@/stores/tokenStore";
 
 import 'vue3-carousel/dist/carousel.css'
 import router from "@/router";
@@ -46,11 +48,11 @@ export default defineComponent({
     Slide,
     Navigation,
   },
-    data(){
-      return{
-          info:""
-      }
-    },
+  data() {
+    return {
+      info: ""
+    }
+  },
   setup() {
     const carousel = ref(null)
 
@@ -65,25 +67,25 @@ export default defineComponent({
     }
 
     const getAndChangeRecipe = async () => {
-        try {
-            let carouselIndex = carousel.value.data.currentSlide._value
-            let recipesResponse = await recipeService.getNewRecipe(tokenStore().user.jwt, recipesShown.value);
-            changeRecipe(carouselIndex, recipesResponse.data)
-        }catch (error){
-            console.log(error)
-        }finally {
-        
-        }
+      try {
+        let carouselIndex = carousel.value.data.currentSlide._value
+        let recipesResponse = await recipeService.getNewRecipe(tokenStore().user.jwt, recipesShown.value);
+        changeRecipe(carouselIndex, recipesResponse.data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+
+      }
     }
 
-    async function addToShoppingList(){
-        this.info="Lagrer til handleliste..."
-        try {
-            await recipeService.addToShoppingList(tokenStore().user.jwt, displayRecipes.value)
-            this.info="Lagret!"
-        }catch (error){
-            console.log(error)
-        }
+    async function addToShoppingList() {
+      this.info = "Lagrer til handleliste..."
+      try {
+        await recipeService.addToShoppingList(tokenStore().user.jwt, displayRecipes.value)
+        this.info = "Lagret!"
+      } catch (error) {
+        console.log(error)
+      }
     }
 
     return {
@@ -97,109 +99,36 @@ export default defineComponent({
   },
   async mounted() {
     let recipeEntities = [];
-        try {
-            let recipesResponse = await recipeService.getWeekMenu(tokenStore().user.jwt);
-            let recipes = recipesResponse.data
-            for (let recipe of recipes) {
-                recipeEntities.push(recipe)
-            }
-        }catch (error){
-            console.log(error)
-        }finally {
-        
-        }
-        this.displayRecipes=recipeEntities
-        this.recipesShown=recipeEntities.slice(0)
+    try {
+      let recipesResponse = await recipeService.getWeekMenu(tokenStore().user.jwt);
+      let recipes = recipesResponse.data
+      for (let recipe of recipes) {
+        recipeEntities.push(recipe)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+
+    }
+    this.displayRecipes = recipeEntities
+    this.recipesShown = recipeEntities.slice(0)
   },
-    created() {
-        if(!tokenStore().user.username){
-            router.push("/")
-        }
+  created() {
+    if (!tokenStore().user.username) {
+      router.push("/")
+    }
+  },
+  computed: {
+    username() {
+      return tokenStore().user.username
     },
-  computed:{
-      username(){
-          return tokenStore().user.username
-      },
-      currentSlide(){
-          return this.carousel.data.currentSlide
-      },
+    currentSlide() {
+      return this.carousel.data.currentSlide
+    },
   }
 })
 </script>
 
 <style scoped>
-.carousel__item {
-  min-height: 10em;
-    padding: 4em;
-  background-color: lightblue;
-  color: black;
-  font-size: 1.2em;
-  border-radius: 1em;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-    background-size:     cover;                      /* <------ */
-    background-repeat:   no-repeat;
-    background-position: center center;
-  max-width: 50em;
-  margin: auto;
-
-}
-
-#carousel{
-  width: 50em;
-  margin: auto;
-}
-
-.carousel__slide {
-  padding: 5px;
-}
-
-.carousel__prev,
-.carousel__next {
-  box-sizing: content-box;
-}
-
-.addToShoppingList {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
- .container{
-   background-color: #f7f7f7;
-   border-radius: 1.25em; /* 20px / 16px = 1.25em */
-   padding: 1.875em; /* 30px / 16px = 1.875em */
-   margin-bottom: 2.5em; /* 40px / 16px = 2.5em */
-   width: 50em;
- }
-img {
-  width: 2em;
-  height: 2em;
-  cursor: pointer;
-}
-#slideTitle{
-    background-color: rgb(76, 75, 75, 0.6);
-    color: white;
-    text-shadow: 2em 2em 2em black;
-    width: 100%;
-}
-#info{
-    text-align: center;
-}
-.ingredients-title {
-  font-size: 2.5em; /* 40px / 16px = 2.5em */
-  font-weight: bold;
-  margin-bottom: 1.25em; /* 20px / 16px = 1.25em */
-  color: #333;
-  text-transform: uppercase;
-  letter-spacing: 0.125em; /* 2px / 16px = 0.125em */
-  text-align: center;
-}
-
-.ingredients-text {
-  color: #555;
-  font-size: 1.25em; /* 20px / 16px = 1.25em */
-  line-height: 1.4;
-}
-
+@import "../assets/style/weeklyMenu.css";
 </style>
