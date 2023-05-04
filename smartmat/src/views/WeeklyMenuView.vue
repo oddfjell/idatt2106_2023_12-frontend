d2acf286f11026cf0fda1f5b4f7a28520d04a8eb
EmilOrv <emiloo@stud.ntnu.no>
2023-05-04 14:54:22 +0200

    Made it possible to save WeekMenues and choose number of recipes

--- smartmat/src/views/WeeklyMenuView.vue
+++ smartmat/src/views/WeeklyMenuView.vue
@@ -1,5 +1,6 @@
 <template>
   <div v-if="username" id="page">
+      <div v-if="this.displayRecipes.length>0">
     <Carousel ref="carousel" :wrap-around="false" :items-to-show="1" id="carousel">
       <Slide v-for="recipe in displayRecipes" :key="recipe">
         <div class="carousel__item" :style="{backgroundImage: 'url(' + recipe.image + ')' }">
@@ -12,16 +13,18 @@
         </button>
       </template>
     </Carousel>
+      <div id="controlPanel">
+          <div id="containerThisWeekMenu">
         <p id="info">{{info}}</p>
-        <div id="serving-wrapper">
-          <button class="incrementbutton" @click="minusServing">-</button>
-          <p id="serving-size"> {{ servings }}</p>
-          <button class="incrementbutton" @click="plusServing">+</button>
-        </div>
         <div class=" Btn addToShoppingList">
             <button class="BlueBtn" @click="addToShoppingList">
                 Legg til varer i handleliste</button>
         </div>
+          </div>
+          <div id="containerNewWeekMenu">
+              <button @click="getNewWeekMenu" class="Btn BlueBtn"> Velg ny ukesmeny</button>
+          </div>
+      </div>
     <div class="container" v-if="this.carousel">
       <h1 class="ingredients-title">Ingredienser</h1>
       <p style="display: none">{{ currentSlide }}</p>
@@ -31,36 +34,60 @@
            v-for="(ingredient, index) in this.displayRecipes[currentSlide._value].ingredients ">{{ ingredient }}</p>
       </div>
     </div>
-
+      </div>
+      <div style="text-align: center" v-else class="container">
+          <h1>Du har ingen ukesmeny enda</h1>
+          <div class="Btn">
+            <button class="BlueBtn" @click="getNewWeekMenu"> Få en ukesmeny!</button>
+          </div>
+      </div>
+      <div v-if="popup">
+          <WeekMenuPopup @newWeekMenu="newWeekMenu" @closePopup="closeThePopup"></WeekMenuPopup>
+      </div>
   </div>
   <div v-else><h1>Please log in</h1></div>
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Carousel, Navigation, Slide } from 'vue3-carousel'
 import recipeService from '../services/recipeService'
 import {tokenStore} from "@/stores/tokenStore";
 
 import 'vue3-carousel/dist/carousel.css'
 import router from "@/router";
+import WeekMenuPopup from "@/components/Common/WeekMenuPopup.vue";
 
 export default defineComponent({
   name: 'Basic',
   components: {
+      WeekMenuPopup,
     Carousel,
     Slide,
     Navigation,
   },
 
   methods: {
-    async loadRecipes() {
-
-    }
+      getNewWeekMenu(){
+          this.popup=true
+      },
+      closeThePopup() {
+          this.popup = false;
+      },
+      async newWeekMenu(data){
+          this.popup=false
+          await this.loadRecipes(data.servings, data.ndays)
+          try{
+              let response = await recipeService.saveRecipes(tokenStore().user.jwt, this.displayRecipes)
+          }catch (error){
+              console.log("error")
+          }
+      }
   },
   data(){
     return{
-      info:""
+      info:"",
+      popup:false,
     }
   },
 
@@ -99,31 +126,23 @@ export default defineComponent({
         changeRecipe(carouselIndex, recipesResponse.data)
       } catch (error) {
         console.log(error)
-      } finally {
-
       }
     }
 
-    const loadRecipes = async () => {
+    const loadRecipes = async (servings, nDays) => {
       let recipeEntities = [];
         try {
-            let recipesResponse = await recipeService.getWeekMenu(tokenStore().user.jwt, servings.value);
+            let recipesResponse = await recipeService.getWeekMenu(tokenStore().user.jwt, servings, nDays);
             let recipes = recipesResponse.data
             for (let recipe of recipes) {
                 recipeEntities.push(recipe)
             }
         }catch (error){
             console.log(error)
-        }finally {
-
         }
         displayRecipes.value=recipeEntities
         recipesShown.value=recipeEntities.slice(0)
     }
-
-    onMounted(() => {
-      loadRecipes()
-    })
 
     async function addToShoppingList(){
         this.info="Lagrer til handleliste..."
@@ -148,24 +167,16 @@ export default defineComponent({
       loadRecipes
     }
   },
-  async mounted() {
-
-    let recipeEntities = [];
-    try {
-      let recipesResponse = await recipeService.getWeekMenu(tokenStore().user.jwt);
-      let recipes = recipesResponse.data
-      for (let recipe of recipes) {
-        recipeEntities.push(recipe)
+    async mounted() {
+      try{
+          let response = await recipeService.getSavedWeekMenu(tokenStore().user.jwt)
+          console.log(response.data)
+          this.displayRecipes = response.data
+      }catch (error){
+          console.log(error)
       }
-    } catch (error) {
-      console.log(error)
-    } finally {
-
-    }
-    this.displayRecipes = recipeEntities
-    this.recipesShown = recipeEntities.slice(0)
-  },
-  created() {
+    },
+    created() {
     if (!tokenStore().user.username) {
       router.push("/")
     }
@@ -183,7 +194,10 @@ export default defineComponent({
 
 <style scoped>
 @import "../assets/style/weeklyMenu.css";
-
+#controlPanel{
+    display: flex;
+    justify-content: space-evenly;
+}
 
 
 </style>
