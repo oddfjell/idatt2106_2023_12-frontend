f6637d7f1dffca9bf15a8ba06d3ecc7865710f00
ingrid <ingrfla@stud.ntnu.no>
2023-05-04 14:25:28 +0200

    cypress test shoppingList + small fix on other cypress tests

--- smartmat/cypress/e2e/weekMenu.cy.js
+++ smartmat/cypress/e2e/weekMenu.cy.js
@@ -1,10 +1,17 @@
 describe('WeekMenu', () => {
     beforeEach(() => {
-        cy.visit("/home")
-        cy.get('input[name=username]').type("jonas");
-        cy.get('input[name=password]').type("jonas");
-        cy.get('#loginBtn').click();
-        cy.visit("/weeklyMenu")
+        cy.fixture('user').then((users) => {
+            const {testUser} = users;
+            cy.visit("/home")
+            cy.get('input[name=username]').type(testUser.username);
+            cy.get('input[name=password]').type(testUser.password);
+            cy.get('#loginBtn').click();
+            cy.contains("testUser")
+            cy.get('#profile-card').click()
+            cy.get('input[type=password]').type(testUser.password)
+            cy.get('button[type=submit]').click()
+            cy.visit("/weeklyMenu")
+        })
     });
 
     it('recipes get displayed', () => {
@@ -16,7 +23,7 @@ describe('WeekMenu', () => {
         cy.get('ol').find('li').first().find('div').first().find('h3').invoke('text').then(h3 => {
             recipeTitle=h3;
         })
-        cy.get('#refreshRecipeBtn').click();
+        cy.get('#refreshRecipeBtn').click({force:true});
         cy.get('ol').find('li').first().find('div').first().find('h3').should('not.include.text', recipeTitle);
     });
 });
